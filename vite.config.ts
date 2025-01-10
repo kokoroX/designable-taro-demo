7f5cf090cc54d68206a668b36dafceb0f80eacfb
kokoroX <8462304@qq.com>
2025-01-10 23:41:54 +0800

    优化配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import vitePluginImp from 'vite-plugin-imp'
 
-
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -40,7 +39,12 @@ export default defineConfig(({ mode }) => {
         {
           find: /^@tarojs\/components$/,
           replacement: '@tarojs/components-react'
-        }
+        },
+        {
+          find: /^@antmjs\/vantui$/,
+          replacement: '@kokoro/antmjs-vantui',
+          // customResolver
+        },
       ]
     }
   }
